427e96a3ed50b2d94fca8a971c8e99250ff68019
Crew Spence <csspence123@gmail.com>
2020-01-12 06:34:46 -0600

    Created function stub for Code Wars toy problem

---
+++ summing_a_numbers_digits.js
@@ -0,0 +1,17 @@
+/*
+Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits. For example:
+
+  sumDigits(10);  // Returns 1
+  sumDigits(99);  // Returns 18
+  sumDigits(-32); // Returns 5
+*/
+function sumDigits(number) {
+}
+
+
+
+//Restate the problem in your own words.
+//What are the inputs?
+//What are the outputs?
+//Can the outputs be determined from the inputs?  Do I already have enough information to solve the problem?  What if you don't have enough information from the user?
+//How should you label the important pieces of data that are a part of the problem?
